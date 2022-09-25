38133cfda8778e81b8253990b11d2aabb51027da
MariaLozano01 <102619474+MariaLozano01@users.noreply.github.com>
2022-09-25 19:20:01 -0400

    update

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def Homepage():
+    return 'Welcome to Homepage'
+
+@app.route('/Careers')
+def Careers():
+    return 'Welcome to the Careers page'
+
+@app.route('/Mission')
+def Mission():
+    return 'This is the Mission of the company'
+
+if __name__ == '__main__': 
+    app.run(debug=True, host='localhost', port=8000)
+
+
 
